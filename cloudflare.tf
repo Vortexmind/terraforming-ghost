3ce72ddb9d22196204a1e4870e9eb5fe307f2ec8
Vortexmind <Vortexmind@users.noreply.github.com>
2020-10-10 13:13:37 +0100

    Tweaks

--- cloudflare.tf
+++ cloudflare.tf
@@ -10,7 +10,7 @@ resource "cloudflare_record" "ghost_blog_record" {
   type    = "A"
   name    = var.ghost_blog_dns
   value   = data.digitalocean_droplet.web.ipv4_address
-  ttl     = "300"
+  ttl     = "1"
 	proxied = true
  }
 
@@ -19,7 +19,7 @@ resource "cloudflare_record" "ghost_blog_record" {
   type    = "A"
   name    = var.commento_dns
   value   = data.digitalocean_droplet.web.ipv4_address
-  ttl     = "300"
+  ttl     = "1"
 	proxied = true
  }
 
@@ -28,7 +28,7 @@ resource "cloudflare_record" "ghost_blog_record" {
   type    = "A"
   name    = var.static_dns
   value   = data.digitalocean_droplet.web.ipv4_address
-  ttl     = "300"
+  ttl     = "1"
 	proxied = true
  }
 
@@ -36,6 +36,10 @@ resource "cloudflare_zone_settings_override" "ghost_zone_settings" {
     zone_id = lookup(data.cloudflare_zones.ghost_domain_zones.zones[0], "id")
     settings {
         always_use_https = "on"
+        automatic_https_rewrites = "on"
+        always_online = "on"
+        http3 = "on"
+        min_tls_version = "1.2"
         brotli = "on"
         minify {
             css = "on"
