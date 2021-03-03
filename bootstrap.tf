552b362de32e544c2fc6038e589321e8ea416d74
Vortexmind <Vortexmind@users.noreply.github.com>
2021-03-03 22:21:40 +0000

    Update pinned terraform provider versions

--- bootstrap.tf
+++ bootstrap.tf
@@ -1,10 +1,10 @@
 provider "digitalocean" {
-  version = "1.22.1"
+  version = "2.5.1"
   token = var.digitalocean_token
 }
 
 provider "cloudflare" {
-  version = "2.11.0"
+  version = "2.18.0"
   api_key = var.cloudflare_api_key
   email = var.cloudflare_email
 }
